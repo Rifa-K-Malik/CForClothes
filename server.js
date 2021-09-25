bf0420746232d4eb8811ecaaf18531fb991bf3c3
Rifa-K-Malik <malikrif07@gmail.com>
2021-09-25 11:21:44 +0530

    initial

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/CforClothes'));
-
+app.use(express.static(__dirname + '/dist/cfor-clothes'));
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/CforClothes/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+  res.sendFile(path.join(__dirname+
+    '/dist/cfor-clothes/index.html'));});
 app.listen(process.env.PORT || 8080);
